37ce9093ffe218cde53385f17335c8d744baa589
joelxue123 <820570376@qq.com>
2024-10-23 09:54:08 +0800

    chore: 删除测试生成的临时文件
    - 移除了 Firmware/Tests 目录下多个测试文件
    - 包括 test_can.o、test_interp.o、test_muliturns.o、test_rotate.o、test_runner.o、test_timer.o 和 test_trap_traj.o
    增加data_race.cpp

---
+++ Firmware/Tests/data_race.cpp
@@ -0,0 +1,21 @@
+#include <thread>
+#include <iostream>
+
+float shared_variable = 0;
+
+void increment_function() {
+    for (int i = 0; i < 1000000; ++i) {
+          shared_variable += 1;
+    }
+}
+
+int main() {
+    std::thread t1(increment_function);
+    std::thread t2(increment_function);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Final value: " << shared_variable << std::endl;
+    return 0;
+}
