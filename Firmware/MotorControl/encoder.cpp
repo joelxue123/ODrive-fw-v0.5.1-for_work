af541f58f548169d85be0cf17f78cac7465fb142
joelxue123 <820570376@qq.com>
2025-01-20 09:59:40 +0800

    refactor(Firmware): 调整编码器初始角度并优化配置流程
    - 修改 Encoder::setup 函数中的磁角度寄存器设置值
    - 注释掉不必要的写 NVM 和延迟操作，简化配置流程

--- Firmware/MotorControl/encoder.cpp
+++ Firmware/MotorControl/encoder.cpp
@@ -49,10 +49,10 @@ void Encoder::setup() {
     osMessageQDef(encoder_queue, 1, sizeof(EncoderCommand));
     encoder_queue_id_ = osMessageCreate(osMessageQ(encoder_queue), NULL);
     MA600_Init(&motor_spi_hardware_);
-    writeMagAlphaRegister(&motor_spi_hardware_,0x02, 161);
+    writeMagAlphaRegister(&motor_spi_hardware_,0x02, 156);
     writeMagAlphaRegister(&motor_spi_hardware_,0x03, 0x01);
-    write_nvm(&motor_spi_hardware_);
-    osDelay(1000);
+    //write_nvm(&motor_spi_hardware_);
+    //osDelay(1000);
 
     mode_ = config_.mode;
     abs_spi_cs_pin_init();
